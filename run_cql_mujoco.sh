dc68d4082420a17b237b9e46053fc3709f48486d
Matthias Weissenbacher <77483344+matthias-weissenbacher@users.noreply.github.com>
2021-05-17 15:01:18 +0900

    Update run_cql_mujoco.sh

--- run_cql_mujoco.sh
+++ run_cql_mujoco.sh
@@ -8,8 +8,7 @@ num_devices=2 #  N=2 number of available GPUS
 declare -a EnvArray=("halfcheetah-medium-expert-v0" "hopper-medium-expert-v0" "walker2d-medium-expert-v0" "halfcheetah-medium-v0" "hopper-medium-v0" "walker2d-medium-v0" "halfcheetah-medium-replay-v0" "hopper-medium-replay-v0" "walker2d-medium-replay-v0" "halfcheetah-random-v0" "hopper-random-v0" "walker2d-random-v0")
 
 #Algorithm
- 
-# Iterate the string array using for loop
+
 
 #Run baseline CQL experiments
 for env in ${EnvArray[@]}; do
